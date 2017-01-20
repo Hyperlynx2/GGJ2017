9011d79ee4851dd25343bb90c02d95fe68983d49
Jack riddell <riddellriddell@gmail.com>
2017-01-21 01:54:43 +1100

    half way through setting up the state machine

--- Jammers/Assets/Scripts/GameStateMachiene/GameStateManager.cs
+++ Jammers/Assets/Scripts/GameStateMachiene/GameStateManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -19,36 +20,52 @@ public class GameStateManager : MonoBehaviour
 		NONE
 	}
 
-	public enum InGameState
-	{
-		ENCODE_MESSAGE,
-		JAM_SIGNAL,
-		DECODE_MESSAGE,
-		WAIT_TURN,
-		NOT_INGAME,
-		NONE
-
-	}
+	public static GameStateManager s_gameStateManager = null;
 
 	public GlobalGameState m_globalGameState;
-	public InGameState m_inGameState;
 
-	public List<GameStateScreen> m_scenes;
+	public bool m_changingState = false;
 
-	public void ChangeScene()
+	public List<GameStateScreen> m_screens;
+
+	public void ChangeScene(GlobalGameState newGameState)
 	{
 
 	}
 
 	public IEnumerator ChangeSceenCoRouteen(GlobalGameState gameState)
 	{
+		m_changingState = true;
 
+		//get old state 
+		GameStateScreen currentState = GetScreen (m_globalGameState);
+
+		//get new state
+		GameStateScreen newState = GetScreen (gameState);
+
+		//transittion from old state
+		yield return StartCoroutine( currentState.OnExit ());
+
+		//transittion into new state
+		yield return StartCoroutine(currentState.OnEnter ());
+
+		m_changingState = false;
 
 	}
 
 	public GameStateScreen GetScreen(GlobalGameState gameState)
 	{
+		foreach( GameStateScreen gameScreen in m_screens)
+		{
+			if(gameScreen.m_targetGlobalGameState == gameState)
+			{
+				return gameScreen;
+			}
+		}
 
+		throw new Exception("state does not exist");
+
+		return null;
 	}
 
 
@@ -56,7 +73,15 @@ public class GameStateManager : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-	
+		if (s_gameStateManager != null && s_gameStateManager != this) 
+		{
+			Destroy(this.gameObject);
+		}
+
+		DontDestroyOnLoad (this.gameObject);
+
+		s_gameStateManager = this;
+
 	}
 	
 	// Update is called once per frame
--- Jammers/Assets/Scripts/GameStateMachiene/GameStateScreen.cs
+++ Jammers/Assets/Scripts/GameStateMachiene/GameStateScreen.cs
@@ -4,18 +4,19 @@ using System.Collections;
 public class GameStateScreen : MonoBehaviour 
 {
 	public GameStateManager.GlobalGameState m_targetGlobalGameState;
-	public GameStateManager.InGameState m_targetInGameState;
 
 	public GameObject m_Screen;
 
 	public virtual IEnumerator OnEnter()
 	{
 
+		return null;
 	}
 
 	public virtual IEnumerator OnExit()
 	{
 
+		return null;
 	}
 
 }
