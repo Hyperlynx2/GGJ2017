04f2b635de78701ed1095dadc4752518987b37db
lfrankel <leonard@leonardfrankel.com>
2017-01-20 23:28:02 +1100

    WIP of basic communication. It’s not building, needs packages? also there’s no way to connect just yet.

---
+++ Jammers/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*Other classes should get the GameManager by calling GameManager::instance()*/
+public class GameManager : Networking.NetworkBehaviour
+{
+	private static GameManager s_instance = null;
+
+	private IList<Player> m_playerList;
+
+	private int m_nextPlayerNum;
+
+	public class Exception : System.Exception
+	{
+		public Exception(string message) :base(Message) {}
+	}
+
+	public static GameManager instance()
+	{
+		if (s_instance == null)
+			throw new Exception("GameManager hasn't been instantiated yet. It needs to be attached as a component!");
+
+		return s_instance;
+	}
+
+	void Awake()
+	{
+		if (s_instance != null)
+			throw new Exception ("GameManager has been used more than once.");
+
+		s_instance = this;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_playerList = new ArrayList<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	[Command]
+	int newPlayerNum()
+	{
+		return m_nextPlayerNum++;
+	}
+
+	[Command]
+	void addPlayer(Player player)
+	{
+		m_playerList.Add(player);
+	}
+
+	[Command]
+	void sendMessageTo(int playerNum, string message)
+	{
+		Player recipient = null;
+		for(int i = 0; recipient == null && i < m_playerList.size(); i++)
+		{
+			if(m_playerList[i].getPlayerNumber() == playerNum)
+				recipient = m_playerList[i];
+		}
+
+		if(recipient != null)
+			recipient.receiveMessage(message);
+	}
+
+}
---
+++ Jammers/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Networking.NetworkBehavior
+{
+	//TODO: LHF: player number. probably needs to be assigned by the server.
+
+	private int m_playerNum;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_playerNum = GameManager::instance().newPlayerNum();
+		GameManager::instance ().addPlayer(this);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	//message come in from the server.
+	[ClientRpc]
+	public void receiveMessage(string message)
+	{
+		GUI.Label(Rect (0, 0, 100, 100), message);
+	}
+
+	[ClientRpc]
+	public int getPlayerNumber()
+	{
+		return m_playerNum;
+	}
+}
