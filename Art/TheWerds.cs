1ac3ec0667b6c9dc44285d9e7f879e9fca99c95c
ThatArtyDouche <kipentheodor@gmail.com>
2017-01-21 22:25:22 +0900

    the "thewords.cs" script and prefab are the new additions

---
+++ Art/TheWerds.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheWerds : MonoBehaviour {
+
+	public static TheWerds tehWERDSofTheLord;
+
+	[System.Serializable]
+	public class arrayArray{
+		public string[] stringArray;
+	}
+
+	public arrayArray[] theGodFatherArray;
+
+	// Use this for initialization
+	void Awake () {
+		if (tehWERDSofTheLord == null) {
+			tehWERDSofTheLord = this;
+		} else {
+			if (tehWERDSofTheLord != this){
+				Destroy(this);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Jammers/Assets/Scripts/TheWerds.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheWerds : MonoBehaviour {
+
+	public static TheWerds tehWERDSofTheLord;
+
+	public class arrayArray{
+		public string[] stringArray;
+	}
+
+	public arrayArray[] theGodFatherArray;
+
+	// Use this for initialization
+	void Awake () {
+		if (tehWERDSofTheLord == null) {
+			this = tehWERDSofTheLord;
+		} else {
+			if (tehWERDSofTheLord != this){
+				Destroy(this);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
