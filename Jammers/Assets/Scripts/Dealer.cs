4f3d1f75665ba67e5466fde541792d59e5c1d5ae
lfrankel <leonard@leonardfrankel.com>
2017-01-22 01:20:28 +1100

    WIP

--- Jammers/Assets/Scripts/Dealer.cs
+++ Jammers/Assets/Scripts/Dealer.cs
@@ -21,8 +21,37 @@ public class Dealer : MonoBehaviour
 	We decide what to do based on what state we're in.*/
 	public void PlayerReady(Player player)
 	{
-		//TODO: do this on all playeres if we've determined that they're all ready to change state.
-		player.gameObject.GetComponent<ClientStateManager>().ChangeState();
+		if(!m_readyPlayers.Contains(player))
+		{
+			m_readyPlayers.Add(player);
+		} 
+
+
+		if(m_readyPlayers.Count == m_players.Count)
+		{
+			m_readyPlayers.Clear();
+
+			//who were we waiting for?
+			switch (m_state)
+			{
+			case State.DESCRIBER:
+				DoJammer(m_describer.GetHand()); //TODO: why not just have a "pass hand to this player" method?
+
+				break;
+			case State.JAMMERS:
+				DoJammer(m_jammers[m_nextJammerNum].GetHand()); //DoJammer will increment m_nextJammerNum and if at the max switch state.
+				break;
+
+			}
+
+
+
+
+			//TODO: do this on all players if we've determined that they're all ready to change state.
+			player.gameObject.GetComponent<ClientStateManager>().ChangeState();
+		}
+
+
 	}
 
 	public class Exception : System.Exception
@@ -33,8 +62,20 @@ public class Dealer : MonoBehaviour
 	/////////
 	//private
 	/////////
-
 	private static Dealer s_instance = null;
+
+	private IList<Player> m_players;
+	private IList<Player> m_readyPlayers;
+
+	private Player m_describer;
+	private Player m_guesser;
+	private IList<Player> m_jammers;
+	private int m_nextJammerNum;
+
+	private State m_state;
+
+	//the list of strings the describer gets to pick from.
+	private IList<string> m_candidates;
 
 	enum State
 	{
@@ -61,14 +102,91 @@ public class Dealer : MonoBehaviour
 	}
 
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-		//TODO: LHF: grab all the Players in the scene and register them.	
+		m_candidates = GenerateCandidates();
+
+		m_players = new List<Player>();
+		foreach(Player player in GameObject.FindObjectsOfType(typeof(Player)))
+		{
+			m_players.Add(player);
+
+			player.SetCandidates(m_candidates);
+		}
+
+		if (m_players.Count < 3)
+			throw new Exception("Cannot play with less than three players.");
+
+		if(m_candidates.Count < m_players.Count - 1)
+			throw new Exception("Not enough candidate words for all the players that need them.");
+
+		m_readyPlayers = 0;
+
+		//totally fresh start, so new roles for all players.
+		m_players[0].SetRole(Player.Role.DESCRIBER);
+		m_describer = m_players[0];
+		m_players[m_players.Count - 1].SetRole(Player.Role.GUESSER);
+		m_guesser = m_players[m_players.Count - 1];
+
+		//the players in the middle are jammers
+		for(int p = 1; p < m_players.Count - 1; p++)
+		{
+			m_players[p].SetRole(Player.Role.JAMMER);
+			m_jammers.Add(p);
+		}
+
+		//give target words to the players that need them
+		{
+			//can't just allocate the targets in the same order as the players know them...
+			List<string> shuffled = new List<string>(m_candidates);
+
+			int c = 0;
+			foreach(Player player in m_players)
+			{
+				//the person guessing doesn't have a target.
+				if(player.GetRole() != Player.Role.GUESSER )
+				{
+					player.SetTarget(shuffled[c]);
+					c++;
+				}
+			}
+		}
+
+		//wait for the describer.
+		m_state = State.DESCRIBER;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
 		
 	}
+
+	private IList<string> GenerateCandidates()
+	{
+		List<string> result = new List<string>();
+
+		//TODO: load up the list from the arrays.
+
+		return result;
+	}
+
+	//thanks to http://stackoverflow.com/questions/273313/randomize-a-listt
+	private static Random rng = new Random();  
+
+	public static void Shuffle<T>(IList<T> list)  
+	{  
+		int n = list.Count;  
+		while (n > 1) {  
+			n--;  
+			int k = rng.Next(n + 1);  
+			T value = list[k];  
+			list[k] = list[n];  
+			list[n] = value;  
+		}  
+	}
+
+	///////////////////
+	//state transitions
+	///////////////////
 }
