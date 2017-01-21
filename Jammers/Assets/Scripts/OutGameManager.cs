b8257e737d016c0d70249c525413138abdb8b9cd
lfrankel <leonard@leonardfrankel.com>
2017-01-21 21:50:49 +1100

    Skeletal OutGame scene. Just uses the out-of-the-box stuff for now.

---
+++ Jammers/Assets/Scripts/OutGameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutGameManager : MonoBehaviour
+{
+	public enum State
+	{
+		SPLASH,
+		CONNECT
+	};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
