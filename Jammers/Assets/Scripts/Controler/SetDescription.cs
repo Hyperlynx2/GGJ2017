082764ef0b569cb91be2e8ca4db724421f2e509b
Jack riddell <riddellriddell@gmail.com>
2017-01-22 14:27:42 +1100

    Made stuff

---
+++ Jammers/Assets/Scripts/Controler/SetDescription.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetDescription : ClientUI<WriteDescription>  
+{
+
+	public int m_CardIndex;
+
+	public InputField m_Input;
+
+	public void OnClick()
+	{
+		TargetState.UpdateDescription (m_Input.text, m_CardIndex);
+	}
+}
---
+++ Jammers/Assets/Scripts/SelectCard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCard : MonoBehaviour 
+{
+	public static int s_iSelectedCard;
+
+	public static bool s_bSelected;
+
+	public int _iTargetInt;
+
+
+
+	public void OnClick()
+	{
+		s_iSelectedCard = _iTargetInt;
+		s_bSelected = true;
+	}
+}
