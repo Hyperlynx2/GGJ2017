75bdb91f0e7b16497f2f729848155837d30d63d8
Jack riddell <riddellriddell@gmail.com>
2017-01-22 15:02:34 +1100

    Made win screen

--- Jammers/Assets/Scripts/Fireball.cs
+++ Jammers/Assets/Scripts/Fireball.cs
@@ -55,6 +55,10 @@ public class Fireball : MonoBehaviour
 
 	public Text _txtPlayerWinner;
 
+	public GameObject _objGameEnd;
+
+	public Text _txtGameWinner;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -72,213 +76,223 @@ public class Fireball : MonoBehaviour
 		return (itarget + iround) % _iTargets.Length;
 	}
 
+
+
 	public IEnumerator RunGame()
 	{
-		//
+		while (true) {
 
-		//loop roiunds 
-		for (int i = 0; i < _iRounds; i++) 
-		{
-			//display round start
-			_objRoundStart.SetActive(true);
+			//loop roiunds 
+			for (int i = 0; i < _iRounds; i++) {
+				//display round start
+				_objRoundStart.SetActive (true);
 
-			_txtRoundStart.text = "Round " + (i + 1);
-
-			GameContinueButton.s_bContiune = false;
-
-			//wait for player to start
-			while (GameContinueButton.s_bContiune == false) 
-			{
-				yield return null;
-			}
-
-			//get candidate set
-
-			//randomly select target card
-			//_iDescribeTarget = Random.Range(0,_strCandidates.Length);
-
-			//set random targets for everyone 
-			for (int j = 0; j < _iTargets.Length -1; j++) 
-			{
-				bool bPasses = false;
-				
-				while(bPasses == false)
-				{
-					bPasses = true;
-
-					_iTargets [j] = Random.Range (0, _iTargets.Length - 1);
-
-					for (int k = 0; k < _iTargets.Length; k++) 
-					{
-						if (_iTargets [j] == _iTargets [k -1] ) {
-							bPasses = false;
-						}
-					}
-				}
-			}
-			//set guesser target
-			_iTargets[0] = _iTargets[_iTargets.Length -1];
-
-			_objRoundStart.SetActive(false);
-
-			_objIntermission.SetActive (true);
-
-			_txtIntermittion.text = "Player " + ( RoundOffset(i,0) + 1) + " Turn";
-
-			GameContinueButton.s_bContiune = false;
-
-			//wait for player to start
-			while (GameContinueButton.s_bContiune == false) 
-			{
-				yield return null;
-			}
-
-			_objIntermission.SetActive (false);
-
-
-
-			//display clue enter
-			_objWriteDescriptions.SetActive(true);
-
-			for (int j = 0; j < _strClues.Length; j++) 
-			{
-				_txtDescriptionNumber.text = (j + 1).ToString();
-
-				GameContinueButton.s_bContiune = false;
-
-				while (GameContinueButton.s_bContiune == false) 
-				{
-					yield return null;
-				}
-
-				_strClues [j] = _txtDescriptionInputField.text;
-
-			}
-
-			_objWriteDescriptions.SetActive(false);
-
-		
-
-			//loop through all jammers
-
-			for (int j = 1; j < _iTargets.Length - 1; j++) 
-			{
-
-				_objIntermission.SetActive (true);
-
-				_txtIntermittion.text = "Player " + ( RoundOffset(i,j) + 1) + " Turn";
+				_txtRoundStart.text = "Round " + (i + 1);
 
 				GameContinueButton.s_bContiune = false;
 
 				//wait for player to start
-				while (GameContinueButton.s_bContiune == false) 
-				{
+				while (GameContinueButton.s_bContiune == false) {
+					yield return null;
+				}
+
+				//get candidate set
+
+				//randomly select target card
+				//_iDescribeTarget = Random.Range(0,_strCandidates.Length);
+
+				//set random targets for everyone 
+				for (int j = 0; j < _iTargets.Length - 1; j++) {
+					bool bPasses = false;
+				
+					while (bPasses == false) {
+						bPasses = true;
+
+						_iTargets [j] = Random.Range (0, _iTargets.Length - 1);
+
+						for (int k = 0; k < _iTargets.Length; k++) {
+							if (_iTargets [j] == _iTargets [k - 1]) {
+								bPasses = false;
+							}
+						}
+					}
+				}
+				//set guesser target
+				_iTargets [0] = _iTargets [_iTargets.Length - 1];
+
+				_objRoundStart.SetActive (false);
+
+				_objIntermission.SetActive (true);
+
+				_txtIntermittion.text = "Player " + (RoundOffset (i, 0) + 1) + " Turn";
+
+				GameContinueButton.s_bContiune = false;
+
+				//wait for player to start
+				while (GameContinueButton.s_bContiune == false) {
 					yield return null;
 				}
 
 				_objIntermission.SetActive (false);
 
 
-				_objSelectJamTarget.SetActive (true);
+
+				//display clue enter
+				_objWriteDescriptions.SetActive (true);
+
+				for (int j = 0; j < _strClues.Length; j++) {
+					_txtDescriptionNumber.text = (j + 1).ToString ();
+
+					GameContinueButton.s_bContiune = false;
+
+					while (GameContinueButton.s_bContiune == false) {
+						yield return null;
+					}
+
+					_strClues [j] = _txtDescriptionInputField.text;
+
+				}
+
+				_objWriteDescriptions.SetActive (false);
+
+		
+
+				//loop through all jammers
+
+				for (int j = 1; j < _iTargets.Length - 1; j++) {
+
+					_objIntermission.SetActive (true);
+
+					_txtIntermittion.text = "Player " + (RoundOffset (i, j) + 1) + " Turn";
+
+					GameContinueButton.s_bContiune = false;
+
+					//wait for player to start
+					while (GameContinueButton.s_bContiune == false) {
+						yield return null;
+					}
+
+					_objIntermission.SetActive (false);
+
+
+					_objSelectJamTarget.SetActive (true);
+
+					SelectCard.s_bSelected = false;
+
+					while (SelectCard.s_bSelected == false) {
+						yield return null;
+					}
+
+					_objSelectJamTarget.SetActive (false);
+
+					_objTypeNewSignal.SetActive (true);
+
+					GameContinueButton.s_bContiune = false;
+					while (GameContinueButton.s_bContiune == false) {
+						yield return false;
+					}
+
+					_strClues [SelectCard.s_iSelectedCard] = _txtNewMessage.text;
+
+					_objTypeNewSignal.SetActive (false);
+
+				}
+
+				_objIntermission.SetActive (true);
+
+				_txtIntermittion.text = "Player " + (RoundOffset (i, _iTargets.Length - 1) + 1) + " Turn";
+
+				GameContinueButton.s_bContiune = false;
+
+				//wait for player to start
+				while (GameContinueButton.s_bContiune == false) {
+					yield return null;
+				}
+
+				_objIntermission.SetActive (false);
+
+				_objGuesserSelect.SetActive (true);
+
 
 				SelectCard.s_bSelected = false;
 
-				while (SelectCard.s_bSelected == false) 
-				{
+				while (SelectCard.s_bSelected == false) {
 					yield return null;
 				}
 
-				_objSelectJamTarget.SetActive (false);
+				_objGuesserSelect.SetActive (false);
 
-				_objTypeNewSignal.SetActive (true);
-
-				GameContinueButton.s_bContiune = false;
-				while(GameContinueButton.s_bContiune == false)
-				{
-					yield return false;
-				}
-
-				_strClues [SelectCard.s_iSelectedCard] = _txtNewMessage.text;
-
-				_objTypeNewSignal.SetActive (false);
-
-			}
-
-			_objIntermission.SetActive (true);
-
-			_txtIntermittion.text = "Player " + ( RoundOffset(i, _iTargets.Length -1) + 1) + " Turn";
-
-			GameContinueButton.s_bContiune = false;
-
-			//wait for player to start
-			while (GameContinueButton.s_bContiune == false) 
-			{
-				yield return null;
-			}
-
-			_objIntermission.SetActive (false);
-
-			_objGuesserSelect.SetActive (true);
-
-
-			SelectCard.s_bSelected = false;
-
-			while (SelectCard.s_bSelected == false) 
-			{
-				yield return null;
-			}
-
-			_objGuesserSelect.SetActive (false);
-
-			int iwinner = 0; 
+				int iwinner = 0; 
 		
-			for (int j = 0; j < _iTargets.Length; j++) 
-			{
-				if (_iTargets [j] == SelectCard.s_iSelectedCard) 
-				{
-					_iScores [RoundOffset(i,j)] += 1;
-					iwinner = j;
+				for (int j = 0; j < _iTargets.Length; j++) {
+					if (_iTargets [j] == SelectCard.s_iSelectedCard) {
+						_iScores [RoundOffset (i, j)] += 1;
+						iwinner = j;
+					}
 				}
-			}
 
-			if (_iTargets [0] == SelectCard.s_iSelectedCard) {
-				_objGuesserWin.SetActive (true);
+				if (_iTargets [0] == SelectCard.s_iSelectedCard) {
+					_objGuesserWin.SetActive (true);
+					GameContinueButton.s_bContiune = false;
+
+					//wait for player to start
+					while (GameContinueButton.s_bContiune == false) {
+						yield return null;
+					}
+					_objGuesserWin.SetActive (false);
+				} else {
+					_objJamWin.SetActive (true);
+
+					_txtPlayerWinner.text = "Player " + (RoundOffset (i, iwinner) + 1).ToString () + " Won The Round";
+
+					GameContinueButton.s_bContiune = false;
+
+					//wait for player to start
+					while (GameContinueButton.s_bContiune == false) {
+						yield return null;
+					}
+					_objJamWin.SetActive (false);
+				}
+
+				_objScoreScreen.SetActive (true);
+
 				GameContinueButton.s_bContiune = false;
 
 				//wait for player to start
-				while (GameContinueButton.s_bContiune == false) 
-				{
+				while (GameContinueButton.s_bContiune == false) {
 					yield return null;
 				}
-				_objGuesserWin.SetActive (false);
-			} else 
-			{
-				_objJamWin.SetActive (true);
 
-				_txtPlayerWinner.text = (RoundOffset(i,iwinner) + 1).ToString();
+				_objScoreScreen.SetActive (false);
 
-				GameContinueButton.s_bContiune = false;
-
-				//wait for player to start
-				while (GameContinueButton.s_bContiune == false) 
-				{
-					yield return null;
-				}
-				_objJamWin.SetActive (false);
 			}
 
-			_objScoreScreen.SetActive (true);
+			//display winner screen
+			int iTopScore = -1 ;
+			int iTopPlayer = 0;
+
+			for (int i = 0; i < _iScores.Length; i++) 
+			{
+				if (_iScores [i] > iTopScore) {
+					iTopScore = _iScores [i];
+					iTopScore = i;
+				}
+
+				_iScores [i] = 0;
+			}
+
+			_objGameEnd.SetActive (true);
+
+			_txtGameWinner.text = "Player " + (iTopPlayer + 1) + " Won The Game";
 
 			GameContinueButton.s_bContiune = false;
 
 			//wait for player to start
-			while (GameContinueButton.s_bContiune == false) 
-			{
+			while (GameContinueButton.s_bContiune == false) {
 				yield return null;
 			}
 
-			_objScoreScreen.SetActive (false);
-
+			_objGameEnd.SetActive (false);
 		}
 	}
 
